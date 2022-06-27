93bb2e3b239657cd1b3ecf1add8dd1cf5d63449c
NickF0217 <ncfoster17@gmail.com>
2022-06-27 15:35:22 -0400

    Code to add entries to DOM in progress

--- src/index.js
+++ src/index.js
@@ -9,14 +9,31 @@ function addToList(listing) {
     mainList.push(listing);
 }
 
+function addToLeftDom(listing) {
+    const entry = document.createElement('div');
+    entry.textContent = `${listing.title}`;
+    entry.className = 'left-entry';
+    leftSide.appendChild(entry);
+}
 
+function addToDetails(listing) {
+    const entry = document.createElement('div');
+    // entry.textContent = `${listing.title}\r\n${listing.description}\r\n
+    // ${listing.dueDate}\r\n${listing.priority}`;
+    entry.textContent = listing.title + listing.description + listing.dueDate + listing.priority;
+    entry.className = 'right-entry';
+    details.appendChild(entry);
+}
 
 const testListing = makeNewListing("Pet cats", "Pet Dash and Molly on their fuzzy lil heads", 
 "June 26, 00:00", "Priority: High");
 addToList(testListing);
+addToLeftDom(testListing);
+addToDetails(testListing);
 
 const anotherTest = makeNewListing("Run", "Go for a run at the greenway. Do the best you can.",
  'June 27, 14:00', "Priority: Low");
  addToList(anotherTest);
 
 console.log(mainList);
+
